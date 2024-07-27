8236ff652338b9b72293f21a59187731b520088d
DimasAditya2 <esadimasadityaa@gmail.com>
2024-07-27 17:25:13 +0700

    finish commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,21 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/ui/FloatingNav";
-import Image from "next/image";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
+
 
 export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={[{name: 'Home', link: '/', icon: <FaHome/>}]}/>
+        <FloatingNav navItems={navItems }/>
         <Hero />
         <Grid/>
+        <RecentProject/>
+        <div className="flex justify-center items-center mb-5 h-10">
+        <p className="text-white opacity-50 text-xs lg:text-sm">inspiration from javascript mastery</p>
+      </div>
       </div>
     </main>
   );
