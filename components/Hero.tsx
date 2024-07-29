be84f67f92c0b3ca317e84007314cb902f0da080
DimasAditya2 <esadimasadityaa@gmail.com>
2024-07-29 11:58:39 +0700

    fix: greeting

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -22,10 +22,7 @@ const Hero = () => {
         <div className="flex justify-center relative my-20 z-10">
           <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
             <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80 w-96">
-              <div className="mb-4 inline-block text-white rounded-sm mr-1">
-                <Greeting />
-              </div>
-            Welcome to My PortFolio
+              Welcome to My PortFolio
             </h2>
             <TextGenerateEffect
               className="text-center text-[40px] md:text-5xl lg:text-6xl"
